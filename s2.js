dd511093ce829fa1444cf02bcd11981d643bf97d
Colin Boyd <boydcoli@gmail.com>
2015-10-05 19:19:47 +1300

    Javascript Practice - s2.js

---
+++ s2.js
@@ -0,0 +1,15 @@
+var avg(x, y) {
+  return (x + y) / 2;
+}
+
+var avgOfThree(a, b, c) {
+  return (a + b + c) / 3;
+}
+
+var isOdd = function (n) {
+  if (n % 2 === 0 ) {
+    return '0';
+  } else {
+    return '1';
+  }
+};
